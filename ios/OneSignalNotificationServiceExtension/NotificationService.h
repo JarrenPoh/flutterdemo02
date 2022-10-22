c3ad68eb1f8827a9b4ae8c963f65a5842ee073f7
Jarren Poh <jarrenpoh@gmail.com>
2022-10-22 19:04:12 +0800

    somethings...-20221022

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by OV on 2022/10/19.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
